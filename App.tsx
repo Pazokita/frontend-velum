ecbb853ee67b8abb4cec97cddac794cb9bb5d8a6
Pazokita <amalia.maturana@gmail.com>
2025-04-29 16:11:02 +0200

    🔁 App.tsx avec affichage de MapScreen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import MapScreen from './src/screens/MapScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <MapScreen />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
