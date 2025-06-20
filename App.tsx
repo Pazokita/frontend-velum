6d8041a7523bfd6d5f9f67ec920e04c5da71c38f
Pazokita <amalia.maturana@gmail.com>
2025-06-20 16:24:49 +0200

    feat(navigation): configure react-navigation avec navigation stack dans App.tsx

--- App.tsx
+++ App.tsx
@@ -1,19 +1,6 @@
-import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import MapScreen from './src/screens/MapScreen';
+import React from "react";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <MapScreen />
-    </SafeAreaView>
-  );
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
